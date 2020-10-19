2c76147a53d21b2f5ec7e20afd5f24f993c35a7c
dlford <dan-github2@dlford.co>
2020-10-19 06:24:32 -0700

    fix e-steps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -734,7 +734,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 93 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 140.2 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -979,7 +979,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -48, -10, -2.24 }
+#define NOZZLE_TO_PROBE_OFFSET { -48, -10, -2.1 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
